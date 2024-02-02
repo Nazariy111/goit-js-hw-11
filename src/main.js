fe74c73191f0404d6033001e42672bb1d0e5b7db
Nazariy111 <gerikgiga1@gmail.com>
2024-02-02 22:23:58 +0200

    upd

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ const refs = {
     btnEl: document.querySelector('.search-btn'),
 };
 
+
+const loader = document.querySelector('.loader-box');
+
 refs.formEl.addEventListener('submit', onFormSubmit);
 refs.formEl.addEventListener('input', e => {
     const searchWord = refs.formEl.elements.word.value.trim();
@@ -22,31 +25,48 @@ refs.formEl.addEventListener('input', e => {
 
 function onFormSubmit(e) {
     e.preventDefault();
+    loader.classList.remove('.is-hiden');
+    
     const galleryParentElem = document.querySelector('.gallery');
     if (galleryParentElem) { galleryParentElem.remove(); };
     
     const galleryParentElemMarkup = '<div class="gallery"></div>';
     refs.galleryEl.insertAdjacentHTML("afterbegin", galleryParentElemMarkup);
-    // simpleLightBox = new SimpleLightbox('.gallery a').refresh();
     const searchWord = e.target.elements.word.value.trim();
-    console.log(`word-${searchWord}-word`); 
     if (searchWord) { 
-        
         searchImage(searchWord)
             .then(data => {
                 let imagesArray = data.hits;
                 if (imagesArray.length > 0) {
+                    // loader.remove();
+                    loader.classList.add('.is-hiden');
                     renderImages(imagesArray);
                     
                     simpleLightBox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 }).refresh();
                 } else {
-                    console.log("Sorry, there are no images matching your search query. Please try again!");
+                    loader.classList.add('.is-hiden');
+                    iziToast.show({
+                        timeout: 5000,
+                        position: 'topCenter',
+                        color: '#d11804',
+                        messageColor: 'white',
+                        titleColor: '#FFFFFF',
+                        iconColor: '#FFFFFF',
+                        message: "Sorry, there are no images matching your search query. Please try again!",
+                    });
                 };
             });
     } else { 
-        
-        alert('tap something');
-        console.log('error');
+        loader.classList.add('.is-hiden');
+        iziToast.show({
+            timeout: 5000,
+            position: 'topCenter',
+            color: '#d11804',
+            messageColor: 'white',
+            titleColor: '#FFFFFF',
+            iconColor: '#FFFFFF',
+            message: "The field is empty. Please enter a word!",
+                    });
     };
 
     refs.formEl.reset();
@@ -83,6 +103,7 @@ function renderImages(imagesArray) {
         </a>`;})
         .join('');
     const gallery = refs.galleryEl.querySelector('.gallery');
+    
     gallery.insertAdjacentHTML('beforeend', markup);
     
 };
